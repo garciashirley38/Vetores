726c2617bc648585b733e46e8705901bf7c85f51
Shirley <100131478+garciashirley38@users.noreply.github.com>
2022-09-16 20:35:43 -0300

    Add files via upload

---
+++ Vetores.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Vetores {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+        String[] letras = { "A", "B", "C", "D", "E" };
+        System.out.println(Arrays.toString(letras));
+    }
+}
---
+++ Vetores2.java
@@ -0,0 +1,22 @@
+public class Vetores2 {
+
+    public static void main(String[] args) {
+        int[] numeros = { 9, 10, 12, 25, 2 };
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+            media += numeros[i];
+        }
+        System.out.println("maior: " + maior);
+        System.out.println("menor: " + menor);
+        System.out.println("media: " + media / numeros.length);
+    }
+}
